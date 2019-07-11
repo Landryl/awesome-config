46c22efcca7a0480a5549b742cd75ccfb1c11909
Landryl <Landryl@users.noreply.github.com>
2019-07-11 16:32:17 +0300

    Adding error handling

--- rc.lua
+++ rc.lua
@@ -3,6 +3,9 @@ local gears = require("gears")
 local awful = require("awful")
 require("awful.autofocus")
 
+-- Error handling
+require("error-handling")
+
 -- Loading environnement
 local env = require("env-config")
 env:init()
@@ -14,6 +17,7 @@ keys:init({env = env})
 -- Load rules
 local rules = require("rules-config")
 rules:init({keys = keys})
+
 
 awful.screen.connect_for_each_screen(function(s)
     awful.tag({"default"}, s, {awful.layout.suit.tile.left})
