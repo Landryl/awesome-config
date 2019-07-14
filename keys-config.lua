44318aa1d7cb7e06161ba50ebecc58dc6e8cf3a0
Landryl <Landryl@users.noreply.github.com>
2019-07-14 11:08:48 +0300

    Adding the ability to move and resize clients using the mouse

--- keys-config.lua
+++ keys-config.lua
@@ -92,6 +92,14 @@ function keys:init(args)
     self.clientbuttons = gears.table.join(
         awful.button({ }, 1, function (c)
             c:emit_signal("request::activate", "mouse_click", {raise = true})
+        end),
+        awful.button({ mod }, 1, function (c)
+            c:emit_signal("request::activate", "mouse_click", {raise = true})
+            awful.mouse.client.move(c)
+        end),
+        awful.button({ mod }, 3, function(c)
+            c:emit_signal("request::activate", "mouse_click", {raise = true})
+            awful.mouse.client.resize(c)
         end)
     )
 end
