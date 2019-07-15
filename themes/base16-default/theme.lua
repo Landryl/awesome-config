0ee440a2c2b32488b841df17b240178ca21de051
Landryl <Landryl@users.noreply.github.com>
2019-07-15 18:14:31 +0300

    Enabling theme reload through args rather than xrdb

--- themes/base16-default/theme.lua
+++ themes/base16-default/theme.lua
@@ -3,7 +3,51 @@ local env = require("env-config")
 
 os.execute("xrdb -merge " .. env.themesdir .. "/base16-default/Xresources")
 
+local base00 = "#181818"
+local base01 = "#282828"
+local base02 = "#383838"
+local base03 = "#585858"
+local base04 = "#b8b8b8"
+local base05 = "#d8d8d8"
+local base06 = "#e8e8e8"
+local base07 = "#f8f8f8"
+local base08 = "#ab4642"
+local base09 = "#dc9656"
+local base0A = "#f7ca88"
+local base0B = "#a1b56c"
+local base0C = "#86c1b9"
+local base0D = "#7cafc2"
+local base0E = "#ba8baf"
+local base0F = "#a16946"
+
 local theme = require("themes.xcolors.theme")
+theme:reload({
+    foreground = base05,
+    background = base00,
+    cursorColor = base05,
+    color0      = base00,
+    color1      = base08,
+    color2      = base0B,
+    color3      = base0A,
+    color4      = base0D,
+    color5      = base0E,
+    color6      = base0C,
+    color7      = base05,
+    color8      = base03,
+    color9      = base08,
+    color10     = base0B,
+    color11     = base0A,
+    color12     = base0D,
+    color13     = base0E,
+    color14     = base0C,
+    color15     = base07,
+    color16     = base09,
+    color17     = base0F,
+    color18     = base01,
+    color19     = base02,
+    color20     = base04,
+    color21     = base06
+})
 
 theme.wallpaper = env.themesdir .. "/base16-default/wallpaper.png"
 
--- themes/xcolors/theme.lua
+++ themes/xcolors/theme.lua
@@ -6,25 +6,21 @@ local dpi = xresources.apply_dpi
 
 local theme = {}
 
-function theme:reload()
-    local xconfig = require("lib.xconfig").get_current_theme()
-
+function theme:reload(theme)
     self.font      = "SauceCodePro Nerd Font Mono 12"
 
-    self.bg_normal     = xconfig["color18"]
-    self.bg_focus      = xconfig["color19"]
-    self.bg_urgent     = xconfig["color01"]
+    self.bg_normal     = theme.color18
+    self.bg_focus      = theme.color19
+    self.bg_urgent     = theme.color01
 
-    self.fg_normal     = xconfig["foreground"]
-    self.fg_focus      = xconfig["color07"]
-    self.fg_urget      = xconfig["foreground"]
+    self.fg_normal     = theme.foreground
+    self.fg_focus      = theme.color07
+    self.fg_urget      = theme.foreground
 
     self.useless_gap   = dpi(3)
     self.border_width  = dpi(0)
-    self.border_normal = xconfig["color18"]
-    self.border_focus  = xconfig["color19"]
+    self.border_normal = theme.color18
+    self.border_focus  = theme.color19
 end
-
-theme:reload()
 
 return theme
